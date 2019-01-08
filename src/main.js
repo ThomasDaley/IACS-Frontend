aa2b208e6aa94bccee7e152e8222c768a1c86419
ＭuＳic″↗ <452521631@qq.com>
2019-01-08 18:38:20 +0800

    添加模板

--- src/main.js
+++ src/main.js
@@ -1,15 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview'
+import axios from 'axios'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
+Vue.use(iView)
 
-/* eslint-disable no-new */
+// 设置基础URL
+axios.defaults.baseURL = ""
+// 设置请求超时时间
+axios.defaults.timeout = 5000
+
+Vue.prototype.$axios = axios
+
 new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
 })
